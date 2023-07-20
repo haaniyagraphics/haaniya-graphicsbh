3795c89ce1c07378da3a21dabef0b99dfec5e718
haaniyagraphics <139790695+haaniyagraphics@users.noreply.github.com>
2023-07-20 20:23:27 +0300

    c

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,8 @@ import Services from "./components/Services/service";
 import About from "./components/About/about";
 import Testimonial from "./components/Testimonial/testimonial";
 import Footer from "./components/Footer/footer";
-import $ from 'jquery';
-import React, { useEffect } from 'react';
+import Works from "./components/Works/works";
 function App() {
-  useEffect(() => {
-    $('.service-button button').on('click', function(){
-      $('.em-service-text p').toggleClass('-expanded');
-      
-      if ($('.em-service-text p').hasClass('-expanded')) {
-        $('.service-button button').html('Collapse Content');
-      } else {
-        $('.service-button button').html('Continue Reading');
-      }
-    });
-  })
   return (
     <div className="App">
         <Header />
