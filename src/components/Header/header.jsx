9ecc103dedc53e5f18cf797d39d7415423dcbcad
haaniyagraphics <139790695+haaniyagraphics@users.noreply.github.com>
2023-07-20 15:52:42 +0300

    Update header.jsx

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
                                     <li><a href="#">Home</a></li>
                                     <li><a href="/#service">Our Services</a></li>
                                     <li><a href="/#about">About Us</a></li> 
-                                    <li><a href="/#our-purpose">Helo! </a></li> 
+                                    <li><a href="/#our-purpose">Hello! </a></li> 
                                     <li><a href="/#contact">Contact Us</a></li>
                                 </ul>
                             </nav>
