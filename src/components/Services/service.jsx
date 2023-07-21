695395aacb80a31abe2bee7445e7fe592c22cc96
haaniyagraphics <139790695+haaniyagraphics@users.noreply.github.com>
2023-07-21 15:58:00 +0300

    Update service.jsx

--- src/components/Services/service.jsx
+++ src/components/Services/service.jsx
@@ -2,6 +2,16 @@ import React from "react";
 
 
 const Services = () => {
+    const shoot = () => {
+        //alert("Great Shot!");
+        $('.em-service-text p').toggleClass('-expanded');
+        if ($('.em-service-text p').hasClass('-expanded')) {
+            $('.service-button button').html('Collapse Content');
+          } 
+        else {
+            $('.service-button button').html('Continue Reading');
+          }
+      }
     return (
 <div class="service-area" id="service">
     <div class="container">
@@ -59,7 +69,7 @@ const Services = () => {
                                 Email Marketing, Social Media Marketing,Social Media Promotions,Sponsor Ads and more  </p>
                         </div>
                         <div class="service-button">
-                             <button> Read More <i class="bi bi-plus"></i> </button>
+                             <button> Learn More <i class="bi bi-plus"></i> </button>
                         </div>
                     </div>
                 </div>
@@ -81,7 +91,7 @@ const Services = () => {
                                  <p>Every Business should have a Unique story behind thier brand identity mark. We help in building brand image and values for your business. We offer comprehensive brand and design services that include research, brand strategy and positioning, visual identity creation, rebranding and web design. </p>
                             </div>
                             <div class="service-button">
-                                 <button> Read More <i class="bi bi-plus"></i> </button>
+                                 <button> Learn More <i class="bi bi-plus"></i> </button>
                             </div>
                     </div>
                 </div>
@@ -103,7 +113,7 @@ const Services = () => {
                                      </p>
                             </div>
                         <div class="service-button">
-                              <button> Read More <i class="bi bi-plus"></i> </button>
+                              <button> Learn More <i class="bi bi-plus"></i> </button>
                         </div>
                     </div>
                 </div>
@@ -126,7 +136,7 @@ const Services = () => {
                                   Printing services offer high-quality prints at competitive prices and fast delivery.</p>
                         </div>
                         <div class="service-button">
-                                <button> Read More <i class="bi bi-plus"></i> </button>
+                                <button> Learn More <i class="bi bi-plus"></i> </button>
                          </div>
                     </div>
                 </div>
@@ -149,7 +159,7 @@ const Services = () => {
                                 Our professional team will help you develop and deliver your website and app in a quick, better, cost-effective and on-time.</p>
                         </div>
                         <div class="service-button">
-                         <button> Read More <i class="bi bi-plus"></i> </button>
+                         <button> Learn More <i class="bi bi-plus"></i> </button>
                          </div>
                     </div>
                 </div>
